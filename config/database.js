0e7abcdca1257b8296528d79a4d73d2c2b0a1a5a
Shaggy Mission <rescuefriendsuio@gmail.com>
2025-06-24 08:52:59 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('user_service_db', 'admin', 'PoolG10.11', {
-  host: 'mysql.cwizczqy6pft.us-east-1.rds.amazonaws.com',
+const sequelize = new Sequelize('user_service_db', 'admin', 'Lis.12345', {
+  host: 'shaggymission-db-server.cd5kwh1iyrrb.us-east-1.rds.amazonaws.com',
   dialect: 'mysql',
   logging: false,
 });
