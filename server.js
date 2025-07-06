5c3fdc271035ddd0d3a6e4932c00b838ce2a04a1
Shaggy Mission <rescuefriendsuio@gmail.com>
2025-07-06 14:25:03 -0500

    refactor: use sync with alter to avoid data loss during development

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const PORT = process.env.PORT || 3010;
 app.listen(PORT, async () => {
   try {
     await sequelize.authenticate();
-    await sequelize.sync();
     console.log(`User Registration Service running on port ${PORT}`);
   } catch (err) {
     console.error('DB connection error:', err);
